b01ec36e897683e465511da98a42230018c2e80f
HoangTu <trantu2000hp@gmail.com>
2023-02-23 20:24:04 +0700

    save_model_stash

---
+++ app/Models/Order/SubOrder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SubOrder extends Model
+{
+    use HasFactory;
+
+    protected $table = "subOrders";
+    protected $fillable = [
+        "quantity",
+        "sub_total",
+        "status"
+    ];
+
+    public function Order(){
+        return $this -> belongsTo(Order::class);
+    }
+
+    public function Product(){
+        return $this -> hasOne(Product::class);
+    }
+}
