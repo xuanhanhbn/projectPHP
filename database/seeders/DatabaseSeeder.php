be06abf2e33b04f05e391a69e8ff40748d7015cc
Towly24 <caonamtq249@gmail.com>
2023-03-06 23:50:18 +0700

    update product

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        //\App\Models\Category\Category::factory(10)->create();
+        \App\Models\Product\Product::factory(50)->create();
     }
 }
