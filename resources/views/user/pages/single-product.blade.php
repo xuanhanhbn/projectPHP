dc4657328b250d87373ed13ca6586f319326be5c
HoangTu <trantu2000hp@gmail.com>
2023-03-16 09:22:34 +0700

    add cart

--- resources/views/user/pages/single-product.blade.php
+++ resources/views/user/pages/single-product.blade.php
@@ -29,16 +29,6 @@
 
     <!-- ***** Main Banner Area Start ***** -->
     <div class="page-heading" id="top">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="inner-content">
-                        <h2>Single Product Page</h2>
-                        <span>Awesome &amp; Creative HTML CSS layout by TemplateMo</span>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
     <!-- ***** Main Banner Area End ***** -->
 
